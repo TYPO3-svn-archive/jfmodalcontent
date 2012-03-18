66bfd79d1c642c3be16554f49bcc00408bcf5a5f
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-18 21:41:39 +0000

    Fix opacity problem for overlay
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jfmodalcontent/trunk@59353 735d13b6-9817-0410-8766-e36946ffe9aa

--- res/jquery/js/jquery.modalContent-0.1.js
+++ res/jquery/js/jquery.modalContent-0.1.js
@@ -12,7 +12,7 @@
 	$.fn.modalContent = function(opts) {
 		var defaults = {
 			inAnimation: 'top',     // from where will the content slide in (top, right, bottom, right)
-			modalFadeDuration: 200, // fade duration for the modal div
+			overlayFadeDuration: 200, // fade duration for the modal div
 
 			inDelay: 0,             // wait until the animation begins
 			inTransition: 'swing',  // transition of the inAnimation
@@ -25,14 +25,8 @@
 
 		var options = $.extend(defaults, opts);
 
-		// there will be no scroll bar...
-		$(this).addClass('modalContent').wrap('<div class="modalContent-outer"></div>').parent().css({
-			width: $(window).width(),
-			height: $(window).height()
-		});
-
 		$(window).resize(function() {
-			$('.modalContent-outer').css({
+			$('.modalContent-modal, .modalContent-overlay').css({
 				width: $(window).width(),
 				height: $(window).height()
 			});
@@ -46,90 +40,106 @@
 			var animateIn = {};
 			var animateOut = {};
 
+			if ($('.modalContent-overlay').length < 1) {
+				$('body').append('<div class="modalContent-overlay"></div>');
+			}
+
+			var $overlay = $('.modalContent-overlay');
+			$overlay.css({
+				width: $(window).width(),
+				height: $(window).height()
+			}).hide();
+
+			$obj.addClass('modalContent').wrap('<div class="modalContent-outer"></div>').parent().css({
+				width: $(window).width(),
+				height: $(window).height(),
+				zIndex: 1
+			});
+
 			// close if click element with class "close"
 			$parent.find('.close').bind("click", function(){
 				$obj.stop();
-				$parent.fadeOut(options.modalFadeDuration).remove();
+				$overlay.fadeOut(options.overlayFadeDuration, function(){
+					$parent.remove();
+				});
 			});
 
 			if (options.inAnimation == 'fade') {
 				$obj.addClass('modalContent-fade').hide();
-				$parent.
-					hide().
+				$overlay.
 					delay(options.inDelay).
-					fadeIn(options.modalFadeDuration, function() {
+					fadeIn(options.overlayFadeDuration, function() {
 						$obj.
 							fadeIn(options.inDuration, options.inTransition).
 							delay(options.outDelay).
 							fadeOut(options.outDuration, options.outTransition, function() {
-								$parent.fadeOut(options.modalFadeDuration, function() {
+								$overlay.fadeOut(options.overlayFadeDuration, function() {
 									$parent.remove();
 								});
 							});
 					});
 			} else if (options.inAnimation == 'slide') {
-				$obj.addClass('modalContent-slide');
-				$parent.
-					hide().
+				$obj.addClass('modalContent-slide').hide();
+				$overlay.
 					delay(options.inDelay).
-					fadeIn(options.modalFadeDuration, function() {
+					fadeIn(options.overlayFadeDuration, function() {
 						$obj.
-							hide().
-							delay(options.inDelay).
 							slideDown(options.inDuration, options.inTransition).
 							delay(options.outDelay).
 							slideUp(options.outDuration, options.outTransition, function() {
-								$parent.parent().remove();
+								$overlay.fadeOut(options.overlayFadeDuration, function() {
+									$parent.remove();
+								});
 							});
 					});
 			} else {
 				switch (options.inAnimation) {
 					case 'bottom' :
-						obj.addClass('modalContent-bottom');
-						offset = obj.offset();
+						$obj.addClass('modalContent-bottom');
+						offset = $obj.offset();
 						var hideBottom = $(document).outerHeight() - offset.top;
 						cssHide = {top: hideBottom};
 						animateIn = {top: 0};
 						animateOut = cssHide;
 						break;
 					case 'left' :
-						obj.addClass('modalContent-left');
-						offset = obj.offset();
-						var hideLeft = (obj.outerWidth()*-1) - offset.left;
+						$obj.addClass('modalContent-left');
+						offset = $obj.offset();
+						var hideLeft = ($obj.outerWidth()*-1) - offset.left;
 						cssHide = {left: hideLeft};
 						animateIn = {left: 0};
 						animateOut = cssHide;
 						break;
 					case 'right' :
-						obj.addClass('modalContent-right');
-						offset = obj.offset();
+						$obj.addClass('modalContent-right');
+						offset = $obj.offset();
 						var hideRight = $(document).outerWidth() - offset.left;
 						cssHide = {left: hideRight};
 						animateIn = {left: 0};
 						animateOut = cssHide;
 						break;
 					default : // default = top
-						obj.addClass('modalContent-top');
-						offset = obj.offset();
-						var hideTop = (obj.outerHeight()*-1) - offset.top;
+						$obj.addClass('modalContent-top');
+						offset = $obj.offset();
+						var hideTop = ($obj.outerHeight()*-1) - offset.top;
 						cssHide = {top: hideTop};
 						animateIn = {top: 0};
 						animateOut = cssHide;
 						break;
 				}
-				$parent.
-					hide().
+				$obj.hide();
+				$overlay.
 					delay(options.inDelay).
-					fadeIn(options.modalFadeDuration, function() {
+					fadeIn(options.overlayFadeDuration, function() {
 						$obj.
 							css(cssHide).
-							hide().
-							delay(options.inDelay).
 							show().
 							animate(animateIn, options.inDuration, options.inTransition).
 							delay(options.outDelay).
 							animate(animateOut, options.outDuration, options.outTransition, function() {
-								$parent.parent().remove();
+								$overlay.fadeOut(options.overlayFadeDuration, function() {
+									$parent.remove();
+								});
 							});
 					});
 			}
