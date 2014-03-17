b6e842e2e23ccdfbcb86bdee741c34562845e404
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-03-17 23:05:30 +0000

    Add compatibility for TYPO3 6.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jfmodalcontent/trunk@84334 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_jfmodalcontent_pi1.php
+++ pi1/class.tx_jfmodalcontent_pi1.php
@@ -27,7 +27,6 @@
  * Hint: use extdeveval to insert/update function index above.
  */
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
 require_once(t3lib_extMgm::extPath('jfmodalcontent').'lib/class.tx_jfmodalcontent_pagerenderer.php');
 
 
